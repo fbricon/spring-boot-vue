ba20a53928597f2347a1fe2d58612d96f53304f4
Fred Bricon <fbricon@gmail.com>
2018-10-22 13:47:33 -0700

    Turn into multi-module
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ todo-model/src/main/java/com/redhat/vscode/demo/model/Todo.java
@@ -0,0 +1,69 @@
+package com.redhat.vscode.demo.model;
+
+import java.util.Map;
+
+/**
+ * Todo class
+ */
+public class Todo {
+
+    private String id;
+    private String title;
+    private boolean completed;
+
+    public Todo(String string, boolean b) {
+        this.title = string;
+        this.completed = b;
+    }
+
+    public Todo() {
+        super();
+    }
+
+    public Todo(String title) {
+        this(title, false);
+    }
+
+    /**
+     * @return the completed
+     */
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @param completed the completed to set
+     */
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+}
